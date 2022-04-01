0bf46ceeec590607417e0a92071d752e5e9cd622
Zoltan Sisak <zoltan.sisak@gmail.com>
2022-04-01 16:17:13 +0200

    script

--- assets/choco-install.cmd
+++ assets/choco-install.cmd
@@ -1,6 +1,12 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString
 choco install googlechrome -y
 choco install vscode -y
 choco install git -y
 git config --system core.longpaths true
 choco install nodejs-lts -y
 choco install firefox -y
+md c:\sj2022-s1-final
+cd sj2022-s1-final
+git clone https://github.com/skillsithu/sj2022-s1-final-01.git .
+copy assets\httpd.conf c:\xampp\apache2\conf\
+#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
